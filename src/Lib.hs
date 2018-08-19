bdf22e82553bef33bbe1142f5f9bcd280637a1c4
Alexandre Krispin <k.m.alexandre@gmail.com>
2018-08-20 00:53:46 +0900

    Add Json reader and writer

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,45 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import Data.Aeson
+-- import Data.ByteString.Lazy.readFile
+import Data.Maybe (fromMaybe)
+import Data.Text.Lazy.IO as T
+import Data.Text.Lazy.Encoding as T
+import Data.Time.ISO8601 (parseISO8601)
+import Data.Time.Format (formatTime)
+import System.Locale (iso8601DateFormat, rfc822DateFormat)
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import GHC.Generics
+
+data Photo =
+  Photo { albumId :: Int
+        , id :: Int
+        , title :: String
+        , url :: String
+        , thumbnailUrl :: String
+        } deriving (Show, Generic)
+
+
+instance FromJSON Photo
+instance ToJSON Photo
+
+
+db :: String
+db = "db.json"
+
+loadDB :: IO B.ByteString
+loadDB = B.readFile db
+
+saveDB :: B.ByteString -> IO ()
+saveDB state =
+      B.writeFile "saved_db.json" state
+
+main =
+  do
+    record <- (eitherDecode <$> loadDB) :: IO (Either String [Photo])
+    case record of
+      Left err -> Prelude.putStrLn err
+      Right rec ->
+        saveDB $ encode rec
